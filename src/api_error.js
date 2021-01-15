0b85ad26ca630c620baa1d79c2a23ba92b27a694
Bruno Gonçalves <bugotech@gmail.com>
2021-01-15 08:29:36 -0300

    #erro ao registrar o erro quando os objetos de erros do js

--- src/api_error.js
+++ src/api_error.js
@@ -1,3 +1,5 @@
+const e = require("cors");
+
 class ApiError
 {
     /**
@@ -9,11 +11,34 @@ class ApiError
     constructor(id, data = {})
     {
         // Verificar se o ID inicia com o prexido de erro.
+        if (typeof id == 'object') {
+            if (id instanceof Error) {
+                Object.assign(data, { 
+                    message: id.message, 
+                    class  : id.constructor.name,
+                    file   : id.fileName,
+                    line   : id.lineNumber
+                });
+            } else if (id instanceof TypeError) {
+                Object.assign(data, { 
+                    message: id.message, 
+                    class  : id.constructor.name,
+                    file   : id.fileName,
+                    line   : id.lineNumber
+                });
+            } else {
+                Object.assign(data, { message: id });
+            }
+
+            id = 'erro.nao.mapeado';
+        }
+
+        // Verificar strings
         if ((typeof id != 'string') || ((typeof id == 'string') && (id.substr(0, 5) != 'erro.'))) {
             Object.assign(data, { message: id });
             id = 'erro.nao.mapeado';
         }
-
+        
         this.id = id;
         this.data = data;
     }
