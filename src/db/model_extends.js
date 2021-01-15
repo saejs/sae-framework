c865fcee85fde41a432744240fc9b181f75ef18b
Bruno Gonçalves <bugotech@gmail.com>
2021-01-15 15:45:33 -0300

    #erro destroy static

--- src/db/model_extends.js
+++ src/db/model_extends.js
@@ -3,7 +3,7 @@ module.exports = (Model) => {
     const __save = Model.prototype.save;
     Model.prototype.__save = __save;
 
-    Model.prototype.save = async function(options) {
+    Model.prototype.save = async function(options = {}) {
 
         // Verificar se deve atribuir a transacao atual
         this.$db.transaction.apply(options);
@@ -15,7 +15,19 @@ module.exports = (Model) => {
     const __destroy = Model.prototype.destroy;
     Model.prototype.__destroy = __destroy;
 
-    Model.prototype.destroy = async function(options) {
+    Model.prototype.destroy = async function(options = {}) {
+
+        // Verificar se deve atribuir a transacao atual
+        this.$db.transaction.apply(options);
+
+        return await this.__destroy(options);
+    }
+
+    // Extender method model "destroy"
+    const __destroyStatic = Model.destroy;
+    Model.__destroy = __destroyStatic;
+
+    Model.destroy = async function(options = {}) {
 
         // Verificar se deve atribuir a transacao atual
         this.$db.transaction.apply(options);
