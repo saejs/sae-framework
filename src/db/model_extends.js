6bf23d7dc55e4148af87a26eea2158cf61dc399f
Bruno Gonçalves <bugotech@gmail.com>
2021-01-20 08:43:09 -0300

    add model hiddens and extend toJSON

--- src/db/model_extends.js
+++ src/db/model_extends.js
@@ -1,10 +1,11 @@
+const arr = require('rhinojs/support/arr');
+
 module.exports = (Model) => {
     /**
      * Extender method model "save"
      */
     const __save = Model.prototype.save;
     Model.prototype.__save = __save;
-
     Model.prototype.save = async function(options = {}) {
 
         // Verificar se deve atribuir a transacao atual
@@ -19,7 +20,6 @@ module.exports = (Model) => {
      */
     const __destroy = Model.prototype.destroy;
     Model.prototype.__destroy = __destroy;
-
     Model.prototype.destroy = async function(options = {}) {
 
         // Verificar se deve atribuir a transacao atual
@@ -27,19 +27,38 @@ module.exports = (Model) => {
 
         return await this.__destroy(options);
     }
-    
+
 
     /**
      * Extender method model "destroy"
      */
     const __destroyStatic = Model.destroy;
     Model.__destroy = __destroyStatic;
-
     Model.destroy = async function(options = {}) {
 
         // Verificar se deve atribuir a transacao atual
         this.db.transaction.apply(options);
 
         return await this.__destroy(options);
+    }
+
+    /**
+     * Extender method model "toJson"
+     */
+    const __toJSON = Model.prototype.toJSON;
+    Model.prototype.__toJSON = __toJSON;
+    Model.prototype.toJSON = function() {
+        var values = Object.assign({}, this.get());
+
+        if (!this.constructor.hiddens) {
+            return values;
+        }
+
+        arr.each(this.constructor.hiddens, (k, v) => {
+            delete values[v];
+        });
+
+
+        return values;
     }
 };
