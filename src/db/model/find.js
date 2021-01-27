d8c72fc13f02fe2f6816880c97afa7e070b8e7ed
Bruno Gonçalves <bugotech@gmail.com>
2021-01-27 09:09:39 -0300

    add transacao nos finds

--- src/db/model/find.js
+++ src/db/model/find.js
@@ -3,6 +3,7 @@ module.exports = (Model) => {
      * Adicionar o evento de controle do find.
      */
     Model.addHook('beforeFind', (options) => {
+        Model.db.transaction.apply(options);
         Model.setWhereContext(Model, options);
     });
 }
