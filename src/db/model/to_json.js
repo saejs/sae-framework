576260dce6c74c24e3a830ba992e6c7d5926ba43
Bruno Gonçalves <bugotech@gmail.com>
2021-02-01 10:55:35 -0300

    add toCustomJSON no model

--- src/db/model/to_json.js
+++ src/db/model/to_json.js
@@ -4,6 +4,13 @@ module.exports = (Model) => {
     const __toJSON = Model.prototype.toJSON;
     Model.prototype.__toJSON = __toJSON;
     Model.prototype.toJSON = function () {
+
+        // Verificar se deve carregar os valores originais ou pro uma regra
+        if (typeof this.toCustomJSON == 'function') {
+            return this.toCustomJSON.call(null, this);
+        }
+
+        // Carregar dados originais
         var values = Object.assign({}, this.get());
 
         if (!this.constructor.hiddens) {
