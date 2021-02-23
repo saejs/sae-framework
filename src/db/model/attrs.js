e52b729f3fa76c62654a2772dc9b432801087694
Bruno Gonçalves <bugotech@gmail.com>
2021-02-23 08:32:52 -0300

    #build 0.15.12 ajuste setAttributes

--- src/db/model/attrs.js
+++ src/db/model/attrs.js
@@ -1,7 +1,11 @@
 module.exports = (Model) => {
     Model.prototype.setAttributes = function (attrs) {
+        if ((attrs == null) || (attrs == undefined)) {
+            return;
+        }
+
         var keys = Object.keys(attrs);
-        
+
         for (var i = 0; i < keys.length; i++) {
             var key = keys[i];
             this[key] = attrs[key];
