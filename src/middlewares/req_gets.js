a383d4e299c3aa7634fe1111a408ac7c1333f97f
Bruno Gonçalves <bugotech@gmail.com>
2021-05-25 16:23:45 -0300

    #build 0.17.5 add req gets header

--- src/middlewares/req_gets.js
+++ src/middlewares/req_gets.js
@@ -3,6 +3,11 @@ const arr = require('rhinojs/support/arr');
 module.exports = () => {
     return (req, res, next) => {
 
+        // Add get header
+        req.header = (key, defValue = null) => {
+            return arr.get(req.headers, key, defValue);
+        };
+
         // Add get query
         req.__query = req.query;
         req.query = (key, defValue = null) => {
