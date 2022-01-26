5ccda27234d5471d2b796c22694cb915e045eb73
bruno.goncalves <bugotech@gmail.com>
2022-01-26 11:43:45 -0300

    0.19.14 correção na validação das regras

--- src/validates/validar.js
+++ src/validates/validar.js
@@ -4,11 +4,11 @@ const _validatorArgs = (ruleOpts, attrName, errors, data) => {
     return [ruleOpts, attrName, errors, data];
 }
 
-const _validarAttrRule = async (value, ruleName, ruleOpts, attrName, errors, data, rules) => {
+const _validarAttrRule = async (value, ruleName, ruleOpts, attrName, errors, data, validatorRules) => {
     const validarValue = (ruleName == 'subitens') ? value : (String((value == undefined) ? '' : value));
 
     // Verificar se regra existe
-    if (typeof rules[ruleName] !== 'function') {
+    if (typeof validatorRules[ruleName] !== 'function') {
         throw new ApiError('erro.validacao.regra.nao.encontrada', ruleName);
     }
 
@@ -16,7 +16,7 @@ const _validarAttrRule = async (value, ruleName, ruleOpts, attrName, errors, dat
     const validatorArgs = _validatorArgs(ruleOpts, attrName, errors, data);
 
     // Executar regra
-    if (!await rules[ruleName](validarValue, ...validatorArgs)) {
+    if (!await validatorRules[ruleName](validarValue, ...validatorArgs)) {
         errors.push({
             error_id : 'erro.validacao.regra.' + ruleName.toLowerCase(),
             attr     : attrName,
@@ -30,26 +30,26 @@ const _validarAttrRule = async (value, ruleName, ruleOpts, attrName, errors, dat
     return true;
 };
 
-const _validarAttrRules = async (value, rules, attrName, errors, data, rules) => {
+const _validarAttrRules = async (value, rules, attrName, errors, data, validatorRules) => {
     var keyRules = Object.keys(rules);
     for (let i = 0; i < keyRules.length; i++) {
         const ruleName = keyRules[i];
         const ruleOpts = rules[ruleName];
 
-        var ret = await _validarAttrRule(value, ruleName, ruleOpts, attrName, errors, data, rules);
+        var ret = await _validarAttrRule(value, ruleName, ruleOpts, attrName, errors, data, validatorRules);
         if (!ret) {
             break;
         }
     }
 };
 
-const _validar = async (data, rules, errors, rules, prefix = '') => {
+const _validar = async (data, rules, errors, validatorRules, prefix = '') => {
     var keys = Object.keys(rules);
     for (var i = 0; i < keys.length; i++) {
         var attr      = keys[i];
         var rulesAttr = rules[attr];
 
-        await _validarAttrRules(data[attr], rulesAttr, prefix + attr, errors, data, rules);
+        await _validarAttrRules(data[attr], rulesAttr, prefix + attr, errors, data, validatorRules);
     }
 };
 
