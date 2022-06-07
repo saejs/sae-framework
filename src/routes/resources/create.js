90d9ea59b81dfdbb21fddb2dc06057b35edb1ee4
bruno.goncalves <bugotech@gmail.com>
2022-06-07 15:31:07 -0300

    #build 0.19.36

--- src/routes/resources/create.js
+++ src/routes/resources/create.js
@@ -2,15 +2,13 @@ module.exports = (app, resource, middlewares) => {
     app.get(resource.uri + '/create', async (req, res) => {
         var obj = new resource.model();
 
-        // Zerar o ID para identiifcar o insert
-        obj.id = null;
-
         // Carregar valores default
         //...
 
         // Verificar se foi implementado uma macro de controller (create)
         await resource.macro('create', [req, res, resource, obj]);
 
-        res.json(obj);
+        // Zerar o ID para identiifcar o insert        
+        res.json(Object.assign({}, obj.toJSON(), { id: null }));
     }, middlewares);
 }
